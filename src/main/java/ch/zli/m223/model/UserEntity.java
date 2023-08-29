7e4c5e8c74ba0bbccaadebbef4fe88b1d2474feb
molotov <63641380+akselj2@users.noreply.github.com>
2023-08-29 14:02:32 +0000

    Fourth Commit: Added JWT Token Authentication. (Not as part of the Application)

---
+++ src/main/java/ch/zli/m223/model/UserEntity.java
@@ -0,0 +1,54 @@
+package ch.zli.m223.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+
+@Entity
+@Table(name="UserEntity")
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Schema(readOnly = true)
+    private Long id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String email;
+
+    public Object getUsername() {
+        return this.username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    };
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    };
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    };
+
+
+}
